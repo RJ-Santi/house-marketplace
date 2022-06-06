0dcbc199ecc926eccf740d89a31e369e7d036c87
RJ-Santi <cr7rs7@gmail.com>
2022-06-06 03:43:32 -0400

    Initial commit.

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkq-CGnFBXvUT_yeo7o7ublYio9XwA6dw",
+  authDomain: "house-marketplace-app-e5f76.firebaseapp.com",
+  projectId: "house-marketplace-app-e5f76",
+  storageBucket: "house-marketplace-app-e5f76.appspot.com",
+  messagingSenderId: "709535047795",
+  appId: "1:709535047795:web:b0c848ee92a71b73ea4a06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
